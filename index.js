5284d636992aac580287e37e80fb50e8350a9909
Coglione <destroyedbywod@gmail.com>
2026-07-11 14:33:27 +0200

    Crear index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+{
+  "name": "dakubot-discord-bot",
+  "version": "1.0.0",
+  "description": "Bot de Discord de DakuBot: bienvenida a nuevos miembros, siempre conectado.",
+  "type": "module",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.16.3",
+    "express": "^4.21.1"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
